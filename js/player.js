4857cc8a6063a4984a10fc1a28e8efdd9d22f6ac
bharathsatheesan <73638534+bharathsatheesan@users.noreply.github.com>
2021-04-06 20:23:44 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,25 @@
+class Player{
+    constructor(){
+
+    }
+
+    getCount(){
+        var playerCountRef = database.ref('playercount');
+        playerCountRef.on('value', function(data){
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+            playercount : count
+        })
+    }
+
+    update(name){
+        var playerIndex = 'Player ' + playerCount;
+        database.ref(playerIndex).set({
+            name : name
+        })
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var gameState=0;
+var playerCount;
+var form;
+var game;
+var player;
+var database;
+
+function setup(){
+    createCanvas(400,400);
+    
+    database = firebase.database();
+
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    
+}
